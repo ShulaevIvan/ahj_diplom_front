bd181c8c5a1c12d0a80839cc98b503919ad27a4a
Shulaev Ivan <mail@ntdservice.ru>
2023-03-09 18:45:51 +0300

    add reg

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,13 @@
 import FromInput from '../components/formInput';
 
 window.addEventListener('DOMContentLoaded', () => {
-    const mainInput = new FromInput();
+    const mainInput = new FromInput('.app-container');
     const url = 'ws://localhost:7070'
     const ws = new WebSocket(url);
 
     ws.addEventListener('open', (e) => {
         console.log('test2')
+        ws.send('test')
     });
 
     ws.addEventListener('error', (e) => {
@@ -17,7 +18,7 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
     ws.addEventListener('message', (e) => {
-
+        ws.send('test')
     });
 
 });
